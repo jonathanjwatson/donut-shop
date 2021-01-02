8b4de33c84d58890b1f7dead3c3dfb0d68520952
MBP16 <jonathanwatson@MBP16s-MacBook-Pro.local>
2021-01-02 09:40:43 -0500

    Added mongoose connection including on connection and on error messages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // 1. Require Express
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 // 2. Create an instance of Express
 const app = express();
 // 3. Set the PORT
@@ -9,6 +10,23 @@ const PORT = process.env.PORT || 8080;
 // 5. Add middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/donut-shop", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose connected successfully.");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: " + err);
+});
 
 // VIEW ROUTES
 app.get("/", (req, res) => {
